e23f117c4ed1ff615075d988ad4319150c29a1f3
Chakir <1008730@hr.nl>
2021-04-17 14:43:23 +0200

    added book + catalog class

---
+++ catalog.py
@@ -0,0 +1,25 @@
+class Catalog:
+    books = []
+
+    def _init_():
+        pass
+    
+    def search():
+        pass
+
+    def advancedSearch():
+        pass
+
+    def addBook():
+        pass
+
+    def removeBook():
+        pass
+
+    def getAvailableBooks():
+        pass
+
+    
+
+
+
