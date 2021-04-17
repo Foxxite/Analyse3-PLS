550a1ea82f360d412c745c6784a71b25b92cdcb4
Raysu <Rayan.B@pm.me>
2021-04-17 14:44:17 +0200

    Loan Administration

---
+++ LoanAdministration.py
@@ -0,0 +1,14 @@
+class LoanAdministration:
+    loans = []
+    
+    def getLoansByUsers(self):
+        pass
+    
+    def addLoan(self):
+        pass
+    
+    def removeLoan(self):
+        pass
+
+class LoanItem():  # <-- call class book and person
+    pass
